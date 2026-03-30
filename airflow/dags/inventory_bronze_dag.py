03b5adef33fef5b0ce7701d63d459f0a429f9939
franadam <francoisadam@hotmail.be>
2026-03-30 19:49:27 +0200

    feat(airflow): add bronze_dag with create bronze schema task

---
+++ airflow/dags/inventory_bronze_dag.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+from spark.jobs.bronze.load_customers_bronze import create_bronze_tables
+
+# Default Args
+default_args = {
+    "owner": "franadam",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "email": "franadam.data@engineers.com",
+    # 'retries': 1,
+    # 'retry_delay': timedelta(minutes=5),
+    "max_active_runs": 1,
+    "dagrun_timeout": timedelta(hours=1),
+    "start_date": datetime(2025, 1, 1)
+}
+
+with DAG(
+    dag_id="bronze_dag",
+    default_args=default_args,
+    description="DAG to load raw data into database",
+    schedule="0 14 * * *",
+    catchup=False
+) as dag_bronze:
+    
+    # Define tasks
+    create_bronze_schema_task = PythonOperator(
+        task_id="create_bronze_schema",
+        python_callable=create_bronze_tables,
+    )
+
+    # Define dependencies
+    create_bronze_schema_task
