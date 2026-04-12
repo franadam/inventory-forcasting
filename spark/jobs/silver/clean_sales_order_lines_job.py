2ca6c6f28c969c0ec9bda384bf55ea465fe90cdb
franadam <francoisadam@hotmail.be>
2026-04-12 20:05:18 +0200

    feat(spark): add clean sales_order_lines job

---
+++ spark/jobs/silver/clean_sales_order_lines_job.py
@@ -0,0 +1,29 @@
+import os
+import logging
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as F
+
+from spark.common.spark_session import build_spark_session
+from spark.common.data_loading import read_postgresql_table, save_into_db
+from spark.common.clean_utils import clean_ids, clean_decimal, clean_cost, standardize_date, standardize_datetime, clean_int, clean_is_active_types, clean_capital_name, trim_lower_column, clean_address, standardize_postal_code
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def clean_sales_order_lines() -> DataFrame:
+    spark = build_spark_session('clean_sales_order_lines')
+    df = read_postgresql_table(spark=spark, schema='bronze', table='sales_order_lines')
+    cleaned_df = clean_ids(df, "order_id")
+    cleaned_df = clean_ids(cleaned_df, "product_id")
+    cleaned_df = clean_cost(cleaned_df, "qty_ordered")
+    cleaned_df = clean_cost(cleaned_df, "qty_fulfilled")
+    cleaned_df = clean_cost(cleaned_df, "unit_price_eur")
+    cleaned_df = clean_cost(cleaned_df, "line_total_eur")
+
+    #save_into_db(schema='silver', table='sales_order_lines', dataframe=cleaned_df, mode='append')
+    save_into_db(schema='staging', table='sales_order_lines', dataframe=cleaned_df)
+    return cleaned_df
+
+if __name__ == "__main__":
+    clean_sales_order_lines()
+    
