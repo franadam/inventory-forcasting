8585fe43cd658811cc6a5ee2abd1599ac31b6db3
franadam <francoisadam@hotmail.be>
2026-04-12 19:57:45 +0200

    feat(airflow): update silver_dag with clean sales_orders task

--- orchestration/dags/silver_dag.py
+++ orchestration/dags/silver_dag.py
@@ -86,6 +86,17 @@ with DAG(
         name="clean_purchase_order_lines",
         verbose=True
     )
+    
+    clean_sales_orders_task = SparkSubmitOperator(
+        task_id="clean_sales_orders",
+        conn_id=SPARK_CONN_ID,
+        application="/opt/project/spark/jobs/silver/clean_sales_orders_job.py",
+        name="clean_sales_orders",
+        verbose=True
+    )
 
     # Define dependencies
-    clean_customers_task >> clean_locations_task >> clean_products_task >> clean_suppliers_task >> clean_inventory_task >> clean_inventory_movements_task >> clean_product_suppliers_task >> clean_purchase_orders_task  >> clean_purchase_order_lines_task
+    clean_customers_task >> clean_locations_task >> clean_products_task >> \
+    clean_suppliers_task >> clean_inventory_task >> clean_inventory_movements_task >> \
+    clean_product_suppliers_task >> clean_purchase_orders_task  >> \
+    clean_purchase_order_lines_task >> clean_sales_orders_task
